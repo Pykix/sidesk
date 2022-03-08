8f0de947367f2f5a74ef387a9abf5de5f3d1b42e
Pyki <py.gil.ludovic@outlook.com>
2022-03-08 09:31:54 +0100

    feat(app): init

---
+++ app/core/urls.py
@@ -0,0 +1,10 @@
+"""core URL Configuration
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'))
+]
