5a7c9746615aa1bf0b5bd3114a22b1e9484e8af6
Pyki <py.gil.ludovic@outlook.com>
2022-05-04 18:17:27 +0200

    feat(requirements): add crytpography to remove error on mac

--- app/projects/data_factory.py
+++ app/projects/data_factory.py
@@ -8,16 +8,17 @@ from .models import Category, Project
 
 
 class ProjectFactory(factory.django.DjangoModelFactory):
-    class Meta: 
+    class Meta:
         model = Project
-        
-    user = factory.Iterator(CustomUser.objects.all())
-    name = factory.Faker('company')
-    summarize = factory.Faker('text', max_nb_chars=80)
-    description = factory.Faker('paragraph', nb_sentences=50, variable_nb_sentences=True)
-    price = factory.Faker('random_number', digits=8, fix_len=False)
-    category = factory.Iterator(Category.objects.all())
-    slug = factory.Faker('company')
-    visible = factory.Faker('boolean', chance_of_getting_true=100)
-    ordered = factory.Faker('boolean', chance_of_getting_true=0)
 
+    user = factory.Iterator(CustomUser.objects.all())
+    name = factory.Faker("company")
+    summarize = factory.Faker("text", max_nb_chars=80)
+    description = factory.Faker(
+        "paragraph", nb_sentences=50, variable_nb_sentences=True
+    )
+    price = factory.Faker("random_number", digits=5, fix_len=False)
+    category = factory.Iterator(Category.objects.all())
+    slug = factory.Faker("company")
+    visible = factory.Faker("boolean", chance_of_getting_true=100)
+    ordered = factory.Faker("boolean", chance_of_getting_true=0)
