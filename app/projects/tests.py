253de6bac4fa5c05ed343a034e91464f7c6a5014
Ludovic Gil <ludovic@meetdeal.fr>
2022-05-06 11:03:15 +0200

    test(projects): adding test for the project model

--- app/projects/tests.py
+++ app/projects/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import email
+from unicodedata import category
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import RequestFactory, TestCase
+
+from .models import Category, Project
+from .views import ProjectListView
+
+
+class ProjectTestCase(TestCase):
+    
+    def setUp(self):
+        user = get_user_model().objects.create(username='test01', email='test@test.fr', password="test")
+        category = Category.objects.create(label="Test",abbreviated="test")
+        Project.objects.create(user=user, name="test", summarize="a resume", description="a desc", price=20000, category=category, slug="blag")
+        
+    def test_project_creation(self):
+        project = Project.objects.get(name="test")
+        self.assertEqual(project.name, 'test')
+        self.assertEqual(project.slug, 'test')
+
+    def test_project_update(self):
+        project = Project.objects.get(name="test")
+        project.name = "new title"
+        project.save()
+        self.assertEqual(project.name, 'new title')
+        self.assertEqual(project.slug, 'new-title')
+        
+    def test_project_list_view_response(self):
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 200)
+        
